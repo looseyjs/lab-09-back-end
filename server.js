14e3a092c6bcfc78db01f7415da7231a16d6e31a
Padmapriya Ganapathi <gpadmapriya@hotmail.com>
2019-07-12 13:06:14 -0700

    Movie image url fixed

--- server.js
+++ server.js
@@ -134,7 +134,7 @@ function Movies(movies) {
   this.overview = movies.overview;
   this.average_votes = movies.vote_average;
   this.total_votes = movies.vote_count;
-  this.image_url = movies.poster_path;
+  this.image_url = 'https://image.tmdb.org/t/p/w500' + movies.poster_path;
   this.popularity = movies.popularity;
   this.released_on = movies.release_date;
 }
